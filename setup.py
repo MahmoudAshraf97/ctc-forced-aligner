5c034a2597324cd5dbd64fa94124c015398496bd
Mahmoud Ashraf <hassouna97.ma@gmail.com>
2024-05-08 12:25:23 +0300

    Fix pip installation to include uroman(#1)

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ setup(
     readme="README.md",
     python_requires=">=3.8",
     author="Mahmoud Ashraf",
+    author_email="hassouna97.ma@gmail.com",
     url="https://github.com/MahmoudAshraf97/ctc-forced-aligner",
     license="CC-BY-NC 4.0",
     packages=find_packages(),
-    install_requires=["transformers", "torchaudio", "torch", ],
+    install_requires=[line.strip() for line in open("requirements.txt")],
     entry_points={
         "console_scripts": ["ctc-forced-aligner=ctc_forced_aligner.align:cli"],
     },
-    package_data={"":["punctuations.lst"]},
+    package_data={"": ["punctuations.lst","uroman/bin/**/*.*","uroman/data/**/*.*","uroman/lib/**/*.*"]},
     include_package_data=True,
 )
