3c07911b5474b0408066de83ed02f83078f1fc61
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-05-28 17:30:53 +0300

    ..

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        "ctc_forced_aligner.ctc_forced_aligner",
+        ["ctc_forced_aligner/forced_align_impl.cpp"],
+        extra_compile_args=['-O3'],
+    )
+]
+
+setup(ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
