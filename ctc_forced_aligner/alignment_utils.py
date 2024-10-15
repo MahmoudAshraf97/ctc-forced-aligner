5b54a7bd24380d1f6572d1ca0d7930dea6ce947b
Joey Ranieri <6362347+SIlver--@users.noreply.github.com>
2024-10-15 10:14:40 -0400

    Update alignment_utils.py based on review

--- ctc_forced_aligner/alignment_utils.py
+++ ctc_forced_aligner/alignment_utils.py
@@ -142,10 +142,8 @@ def generate_emissions(
         time_to_frame(context_length) : -time_to_frame(context_length) + 1,
     ]  # removing the context
     emissions = emissions.flatten(0, 1)
-    if extention > 0:
-        new_emissions = emissions[: -time_to_frame(extention / SAMPLING_FREQ), :]
-        if new_emissions.size(0) > 0:  # Only reassign emissions if the new tensor is not empty
-            emissions = new_emissions
+    if time_to_frame(extention / SAMPLING_FREQ) > 0:
+        emissions = emissions[: -time_to_frame(extention / SAMPLING_FREQ), :]
 
     emissions = torch.log_softmax(emissions, dim=-1)
     emissions = torch.cat(
